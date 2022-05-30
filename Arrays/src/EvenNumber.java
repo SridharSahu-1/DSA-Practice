f38af1013b543ea671bcb8176950e0c10df7ffd3
Sridhar <sridharsahu5555@gmail.com>
2022-05-30 13:10:42 +0530

    More Array Practice

---
+++ Arrays/src/EvenNumber.java
@@ -0,0 +1,11 @@
+class EvenNumber {
+    public int findNumbers(int[] nums) {
+       int count = 0;
+        for(int num : nums){
+            int noOfDigits = (int)(Math.log10(num))  + 1;
+            if(noOfDigits % 2 == 0)
+                count++;
+        } 
+        return count;
+}
+}
